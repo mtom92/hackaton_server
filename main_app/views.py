46b0b3991633c29b77e8192539ffb1aafad0dfdf
mtom92 <torresmoranm@gmail.com>
2019-05-31 11:06:48 -0700

    validates the risk from the model result

--- main_app/views.py
+++ main_app/views.py
@@ -4,16 +4,25 @@ from .serializers import ChatSerializer
 from django.http import HttpResponse
 import pickle
 import sklearn
-
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 model = pickle.load(open('model.sav', 'rb'))
+
+
+
 
 # Create your views here.
 @api_view(['GET', 'POST'])
 def chat(request):
+    risk = 0
+    level1 = 0
+    level2 = 25
+    level3 = 50
+    level4 = 75
+    level5 = 100
 
     if request.method == 'GET':
         return Response("hello, get")
@@ -23,7 +32,15 @@ def chat(request):
         query = request.data["message"]
         model_input = [query]
         prediction = model.predict_proba(model_input)[0]
-        proba_sad = prediction[0]
-        proba_suicide = prediction[1]
-        response = {"sad": proba_sad, "suicide": proba_suicide}
+        proba_suicide = round(prediction[1]*100)
+
+        if proba_suicide > level1 and proba_suicide <= level2:
+            risk = 1
+        if proba_suicide > level2 and proba_suicide <= level3:
+            risk = 2
+        if proba_suicide > level3 and proba_suicide <= level4:
+            risk = 3
+        if proba_suicide > level4 and proba_suicide <= level5:
+            risk = 4
+        response = {"risk": risk}
         return Response(response)
