ab3287429a5019b8f88ea9bf49ad13a2f836d7ce
mtom92 <torresmoranm@gmail.com>
2019-05-31 08:49:17 -0700

    model imported and returning a response to the client

--- main_app/views.py
+++ main_app/views.py
@@ -2,19 +2,28 @@ from django.shortcuts import render
 from .models import Chat
 from .serializers import ChatSerializer
 from django.http import HttpResponse
-import joblib
-
+import pickle
+import sklearn
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+model = pickle.load(open('model.sav', 'rb'))
+
 # Create your views here.
 @api_view(['GET', 'POST'])
 def chat(request):
+
     if request.method == 'GET':
-        the_model = joblib.load('filename.pkl')
-        response = the_model.grapher("Very stress")
-        return Response(response)
+        return Response("hello, get")
+
     elif request.method == 'POST':
-        return Response("hello, post")
+        print("this is thet data",request.data["message"])
+        query = request.data["message"]
+        model_input = [query]
+        prediction = model.predict_proba(model_input)[0]
+        proba_sad = prediction[0]
+        proba_suicide = prediction[1]
+        response = {"sad": proba_sad, "suicide": proba_suicide}
+        return Response(response)
