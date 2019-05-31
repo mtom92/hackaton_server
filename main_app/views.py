39a280dff4dd10e4963f1ebf9fd18961184ee219
mtom92 <torresmoranm@gmail.com>
2019-05-31 14:04:43 -0700

    added changes on the model results processing to hit all the levels

--- main_app/views.py
+++ main_app/views.py
@@ -18,10 +18,11 @@ model = pickle.load(open('model.sav', 'rb'))
 @api_view(['GET', 'POST'])
 def chat(request):
     risk = 0
-    level1 = 0
-    level2 = 25
-    level3 = 50
-    level4 = 75
+    level0 = 10
+    level1 = 28
+    level2 = 40
+    level3 = 58
+    level4 = 77
     level5 = 100
 
     if request.method == 'GET':
@@ -33,14 +34,16 @@ def chat(request):
         model_input = [query]
         prediction = model.predict_proba(model_input)[0]
         proba_suicide = round(prediction[1]*100)
-
-        if proba_suicide > level1 and proba_suicide <= level2:
+        if proba_suicide < level0:
             risk = 1
-        if proba_suicide > level2 and proba_suicide <= level3:
+        if proba_suicide > level1 and proba_suicide <= level2:
             risk = 2
-        if proba_suicide > level3 and proba_suicide <= level4:
+        if proba_suicide > level2 and proba_suicide <= level3:
             risk = 3
-        if proba_suicide > level4 and proba_suicide <= level5:
+        if proba_suicide > level3 and proba_suicide <= level4:
             risk = 4
+        if proba_suicide > level4 and proba_suicide <= level5:
+            risk = 5
+        print(proba_suicide)
         response = {"risk": risk}
         return Response(response)
